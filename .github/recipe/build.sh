38156404aca95146702473a43d500bdc48212623
Daniel Nachun <daniel.nachun@gmail.com>
2024-12-01 02:12:47 -0800

    fix recipe (#51)

--- .github/recipe/build.sh
+++ .github/recipe/build.sh
@@ -2,7 +2,7 @@
 
 set -o xtrace -o nounset -o pipefail -o errexit
 
-PY_VER=$(python -V | cut -d ' ' -f 2 | cut -d '.' -f 1,2)
+PY_VER=$(python3 -V | cut -d ' ' -f 2 | cut -d '.' -f 1,2)
 SP_DIR=${PREFIX}/lib/python${PY_VER}/site-packages
 mkdir -p ${SP_DIR}
 mkdir -p ${PREFIX}/bin
